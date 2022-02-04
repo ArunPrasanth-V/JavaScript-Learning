1bae704f823dd726784c7ba240dedf99e5b0022e
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-02-04 11:47:12 +0530

    Create conditionals.js

---
+++ codes/conditionals.js
@@ -0,0 +1,51 @@
+
+if(1 ==1)
+{
+    console.log("It's True !!");
+}
+
+// exampel 2
+
+let rating=3;
+if(rating ==3)
+{
+    console.log("You're SuperStar!! ");
+}
+
+// example 3
+let num = 37;
+if(num%2==0)
+ {
+     console.log("Even Number");
+ }
+ else 
+ {
+     console.log("Odd Number !! ");
+ }
+
+
+ //switch :
+ let day=3;
+ switch(day){
+case 1:
+     console.log("Monday");
+     break;
+case 2:
+     console.log("TuesDay");
+     break;
+case 3:
+     console.log("WednesDay");
+     break;
+case 4:
+     console.log("WednesDay");
+     break;
+default:
+    console.log("Some other day");
+   
+ }
+
+ // ternary operator
+ let name='arun';
+ name =='arun'?true:false;
+
+ 
