11735af063a14956587d3ea4018b13ba008a5a8b
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-02-05 16:56:08 +0530

    Create looping.js

---
+++ codes/looping.js
@@ -0,0 +1,21 @@
+// Loops :
+
+
+//1 for Loopd:
+for(let i=0;i<10;i++)
+{
+    console.log(i);
+}
+let m=0;
+while(m++<10){
+    console.log("m ->",m); 
+}
+
+for(let i=0;i<10;i++)
+{
+    for(let j=10;j>i;j--)
+    {
+        console.log(`${i} `);
+    }
+    console.log();
+} 
