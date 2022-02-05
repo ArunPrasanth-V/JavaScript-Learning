3cb1654bbd715558f8213f61b510ec99ea4c358d
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-02-05 12:57:55 +0530

    Create objects.js

---
+++ codes/objects.js
@@ -0,0 +1,32 @@
+// object's
+
+//object literals
+const student={
+    name:"Arun",
+    dept:"CSE",
+    year:3,
+    passion:"Development",
+    programming:['java','javaScript']
+};
+
+console.log(student.name,student.dept);
+
+// here all keys are automatically converted into string
+
+const number={
+    num:"one hundred",
+    "16":"sixteen"
+};
+number['10']='ten';
+
+const stu={
+    firstName:'arun',
+    lastname:"chuck",
+    strengths:student.passion,
+    exams:{
+        midterm:92,
+        final:88
+    }
+};
+
+let avg =(stu.exams.final + stu.exams.midterm)/2;
