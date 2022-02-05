53691f91da857a048028c9e23efba500f1b3e0b0
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-02-05 11:49:25 +0530

    Create Arrays.js

---
+++ codes/Arrays.js
@@ -0,0 +1,26 @@
+// Arrays and objects
+// let student =[];
+let shoppingList=['cheese','coce','ice'];
+console.log(shoppingList[0]);
+let contain=['arun',12,true,null]
+new Array(1,2,3,4,5,6);
+
+let topSong=[
+    "First time ever i saw your face",
+    "God Only Knows",
+    "A day in the life",
+    "lifr on ,mars"
+];
+
+topSong.push('Fortunate son');
+
+
+
+
+let fruits=['apple','banana'];
+let veggies=['asparagus','brussle sprountes'];
+
+console.log(fruits.concat(veggies));
+let meals=['steak','chiken breast'];
+
+let allTogether=fruits.concat(veggies,meals);
