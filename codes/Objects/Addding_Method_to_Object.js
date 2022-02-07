1c37c64e6a36aefa6e827a31968892e17a30e161
ArunPrasanth-V <88626504+ArunPrasanth-V@users.noreply.github.com>
2022-02-07 19:31:08 +0530

    Create Addding_Method_to_Object.js

---
+++ codes/Objects/Addding_Method_to_Object.js
@@ -0,0 +1,27 @@
+//Addding Method to Object
+
+const math={
+    multiply : (x,y)=>{
+        return x*y;
+    },
+    divide :(x,y)=>{
+        return x/y;
+    },
+    square:(x)=>{
+        return x*x;
+    }
+};
+
+// sorted version
+
+const mathV2={
+    multiply(x,y){
+        return x*y;
+    },
+    divide(x,y){
+        return x/y;
+    },
+    square(x){
+        return x*x;
+    }
+};
